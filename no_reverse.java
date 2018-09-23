888f8018687bbdb460cf3ee339896c87bb3a7e69
pokovenkat <42380498+pokovenkat@users.noreply.github.com>
2018-09-23 09:47:10 +0530

    to get reverse of a number

---
+++ no_reverse.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class factorial {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+        int input=sc.nextInt();
+        while(input>0){
+        
