fcd61c14c99382f34a695cefc8b6e011cb0f8c74
pokovenkat <42380498+pokovenkat@users.noreply.github.com>
2018-09-23 09:49:25 +0530

    to reverse the given number

--- no_reverse.java
+++ no_reverse.java
@@ -6,5 +6,15 @@ public class factorial {
 		// TODO Auto-generated method stub
 		Scanner sc=new Scanner(System.in);
         int input=sc.nextInt();
+	int rev=0;
         while(input>0){
+		int r=input%10;
+		rev=(rev*10)+r;
+		input=input/10;
+	}
+	        System.out.println(rev);
+	}
+
+}
+
         
