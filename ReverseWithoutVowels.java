ec809e66d68f58444d7deabfacc4b1bb7561b4dd
pokovenkat <42380498+pokovenkat@users.noreply.github.com>
2018-09-24 19:36:39 +0530

    reverse the given string and remove vowels from it

---
+++ ReverseWithoutVowels.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class ReverseWithoutVowels {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		String str=sc.nextLine();
+		StringBuilder sb=new StringBuilder(str);
+		str=sb.reverse().toString();
+		str=str.replaceAll("a","");
+		str=str.replaceAll("e","");
+		str=str.replaceAll("i","");
+		str=str.replaceAll("o","");
+		str=str.replaceAll("u","");
+		str=str.replaceAll("A","");
+		str=str.replaceAll("E","");
+		str=str.replaceAll("I","");
+		str=str.replaceAll("O","");
+		str=str.replaceAll("U","");
+		System.out.println(str);
+	}
+
+}
