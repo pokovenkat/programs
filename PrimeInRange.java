c2b821c4b828924ad74f73ef01f678f5094c500d
pokovenkat <42380498+pokovenkat@users.noreply.github.com>
2018-09-24 18:42:55 +0530

    count of prime numbers within range

---
+++ PrimeInRange.java
@@ -0,0 +1,36 @@
+
+
+import java.util.Scanner;
+
+public class PrimeInRange {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int l=sc.nextInt();
+		int r=sc.nextInt();
+		int count=0;
+		for(int i=l;i<=r;i++) {
+			if(prime(i)) {
+				count+=1;
+			}
+		}
+		System.out.println(count);
+	}
+	public static boolean prime(int n) {
+		if(n==2)
+			return true;
+		if(n==1)
+			return true;
+		if(n>2) {
+			for(int i=2;i<=n/2;i++) {
+				if(n%i==0) {
+					return false;
+				}
+			}
+			return true;
+		}
+		return false;
+	}
+
+}
