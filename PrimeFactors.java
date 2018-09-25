850cdc4fb599898d6089768b399e082d0e32e0b8
pokovenkat <42380498+pokovenkat@users.noreply.github.com>
2018-09-25 17:59:34 +0530

    to print prime factors in sorted order for i/p no.

---
+++ PrimeFactors.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class PrimeFactors {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc =new Scanner(System.in);	
+		int n=sc.nextInt();
+		for(int i=2;i<n;i++) {
+			if(n%i==0) {
+				if(isprime(i)) {
+					System.out.println(i);
+				}
+			}
+		}
+	}
+	public static boolean isprime(int n) {
+		if(n==2)
+			return true;
+		if(n>2) {
+			for(int i=2;i<=n/2;i++) {
+				if(n%i==0) {
+					return false;
+				}
+			}
+			return true;
+		}
+		return false;
+	}
+
+
+}
