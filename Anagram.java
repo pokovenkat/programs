69bfb2e1124ad52dcf9555453dbd6c77ff84dec1
pokovenkat <42380498+pokovenkat@users.noreply.github.com>
2018-09-25 17:40:27 +0530

    to print count of anagrams in array of strings

---
+++ Anagram.java
@@ -0,0 +1,46 @@
+
+import java.util.Scanner;
+
+public class anagram {
+public static void main(String[] args) {
+	Scanner sc =new Scanner(System.in);	
+	int n=sc.nextInt();
+	int count=0;
+	String input[]=new String[n];
+	for(int i=0;i<n;i++) {
+		input[i]=sc.next();
+	}
+	for(int i=0;i<n;i++) {
+		int l1=input[i].length();
+		for(int j=i+1;j<n;j++) {
+			int l2=input[j].length();
+			if(l1==l2) {
+				if(isAnagram(input[i],input[j])) {
+					count++;
+				}
+			}
+		}
+		
+	}
+	System.out.println(count);
+}
+public static int[] countLetters(String s) {
+	int letter[]=new int [26];
+	for(int i=0;i<s.length();i++) {
+		char c=s.charAt(i);
+		int index=c-'a';
+		letter[index]++;
+	}
+	return letter;
+}
+public  static boolean isAnagram(String s1,String s2) {
+	int l1[]=countLetters(s1);
+	int l2[]=countLetters(s2);
+	for(int i=0;i<26;i++) {
+		if(l1[i]!=l2[i]) {
+			return false;
+		}
+	}
+	return true;
+}
+}
