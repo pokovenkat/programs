714a2135fa35fd47199df55423641a30c97cd577
pokovenkat <42380498+pokovenkat@users.noreply.github.com>
2018-09-26 19:46:47 +0530

    how many ways can 1's and 2's occur in a no.

---
+++ NumWays.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class NumWays {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int count=0;
+		count=n/2+1;
+		System.out.println(count);
+	}
+
+}
